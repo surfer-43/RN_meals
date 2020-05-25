9d31600412d0584f6a72cd4697496b70dd57138f
Joel Brown <surfer_43@hotmail.com>
2020-05-25 11:47:09 -0400

    adding navigation to the app"

---
+++ screens/FiltersScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { 
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+const FiltersScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default FiltersScreen;
+
