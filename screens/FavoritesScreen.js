244988aa2eb6d9316af5b5ea878e4b5b53f2ff44
Joel Brown <surfer_43@hotmail.com>
2020-05-27 14:44:04 -0400

    adding fake content to the favs screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { 
-    Text, 
-    View,
-    StyleSheet,
-} from 'react-native';
+import MealList from '../components/MealList';
 
-const FavoritesScreen = () => {
-    return (
-        <View>
-            <Text>This is our favourites screen</Text>
-        </View>
-    )
+// get data
+import { CATEGORIES } from '../data/dummy-data';
+import { MEALS } from '../data/meals';
+console.log("these are the meals: ", MEALS);
+const catId = 'c1';
+// setting up fake favs
+const favs = MEALS.filter( meal => {
+  return meal.categoryIds.indexOf(catId) >= 0;
+})
+const FavoritesScreen = (props) => {
+  console.log("this is the category we want: ", catId);
+  console.log("these are the meals we will use: ", favs);
+  return <MealList 
+    category={catId}
+    listData={favs}
+    navigation={props.navigation}
+    />
 }
 
-const styles = StyleSheet.create({});
 
 export default FavoritesScreen;
 
